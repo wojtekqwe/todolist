088ea8f77f30a1a1e27f39e51e590d2d4bfe8018
Wojciech Pasternak <wojciech.pasternak@ttsw.com.pl>
2022-07-11 09:54:31 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import { Task } from './task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo';
+  taskName = '';
+  config: { [key: string]: string } | null = null;
+  tasks: Task[] = [
+    {
+      name: 'Siatkówka',
+      done: false,
+    },
+    {
+      name: 'Sprzątanie',
+      done: false,
+    },
+    {
+      name: 'Mycie samochodu',
+      done: false,
+    },
+  ];
+
+  constructor() {
+    setTimeout(() => {
+      this.config = {
+        title: 'Lista zadań',
+        footer: 'Lista zadań zbudowana w Angularze',
+        date: new Date().toDateString(),
+      };
+    }, 500);
+  }
+
+  clearTasks() {
+    this.tasks = [];
+  }
+
+  createTask() {
+    const task: Task = {
+      name: this.taskName,
+      done: false,
+    };
+    this.tasks.push(task);
+    this.taskName = '';
+  }
+
+  deleteTask(task: Task) {
+    this.tasks = this.tasks.filter((e) => e !== task);
+  }
 }
